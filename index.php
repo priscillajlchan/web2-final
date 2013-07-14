49414fa14ba93db8323d62dd14f55a982cbd44d1
Priscilla <priscillachan__@hotmail.com>
2013-07-14 16:11:52 -0700

    First Commit
    Setting up the basic skeleton of my website.
    HTML & CSS

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>"Campaign Title"</title>
+	<link type="text/css" rel="stylesheet" media="screen" href="css/main.css">
+</head>
+
+
+<body>
+
+	<div id="container"> <!-- #container - the whole container -->
+		<header> <!-- #logo & header -->
+			<h1>CAMPAIGN TITLE</h1>
+		    <div id="logo"></div>	
+		</header>
+	</div>
+
+	<div id="mainnav"> <!-- #mainnav - the 4 "tabs" -->
+		<nav>
+			<ul>
+				<li>About</li>
+				<li>Contact</li>
+				<li>Poll</li>
+				<li>Results</li>
+			</ul>
+		</nav>
+	</div>
+
+	<div id="socmed"> <!-- #socmed - social media icons!  -->
+		<ul>
+				<li id="twitter"><a href="http://twitter.com/mozilladrumbeat">Twitter</a></li>
+				<li id="facebook"><a href="http://www.facebook.com/mozilladrumbeat">Facebook</a></li>
+				<li id="flickr"><a href="http://www.flickr.com/">Flickr</a></li>
+			</ul>
+		</div>
+
+
+<section>
+	<article>
+
+	</article>
+</section>
+
+		<footer>
+			<p>Copyright info goes here.</p>
+		</footer>
+
+	</div>
+</body>
+</html>
